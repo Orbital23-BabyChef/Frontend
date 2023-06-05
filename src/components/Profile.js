b2779d6a2f7284d117ae567e9bb0a3f6b819bca4
Kok Liang <limkokliang01@gmail.com>
2023-06-05 17:45:39 +0800

    Added Delete button per post in profile view

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,11 +2,15 @@ import React from "react"
 import { useLocation, useNavigate, Link } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import axios from "axios"
+import { confirmAlert } from 'react-confirm-alert'
+import 'react-confirm-alert/src/react-confirm-alert.css';
 
 
 function Profile (){
     const location = useLocation()
     const username = location.state.currUser
+    
+    const history = useNavigate()
 
     const [recipeList, setRecipeList] = useState([])
     const [searchInput, setSearchInput] = useState("");
@@ -15,7 +19,41 @@ function Profile (){
         e.preventDefault();
         setSearchInput(e.target.value);
     };
-      
+    
+    const submit = (id) => {
+        confirmAlert({
+            title: 'Delete Confirmation',
+            message: 'Are you sure you would like to delete this recipe? This action is irreversible.',
+            buttons: [
+              {
+                label: 'OK',
+                onClick: () => {
+                    try {
+                        axios.post("https://baby-chef.herokuapp.com/delete/", { id })
+                        .then(res => {
+                            if (res.data == "deleteSuccess") {
+                                alert("Record successfully deleted.")
+                                history("/home", {state:{id:username}})
+                            } else {
+                                alert("Error!")
+                            }
+                        })
+                        .catch(e => {
+                            alert("Error!")
+                            console.log(e)
+                        })
+                    } catch (e) {
+                        console.log(e)
+                    }
+                }
+              },
+              {
+                label: 'Cancel',
+              }
+            ]
+        });
+    }
+
     useEffect(() => {
         if (searchInput.length == 0) {
             axios.post("https://baby-chef.herokuapp.com/myRecipes", { username })
@@ -49,6 +87,8 @@ function Profile (){
                         <p>{value.description} </p>
                         <p>Creator: {value.creator}</p> 
                         <Link to={`/edit/${value._id}`} state={{currUser: username}}>Edit</Link>
+                        <br></br>
+                        <button onClick={() => submit(value._id)}>Delete</button>
                     </div>
                 })}
         </div>
